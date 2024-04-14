571be3d6d03912ec27653b635390861b847c820e
invertime <invertime.off@gmail.com>
2024-04-14 14:48:01 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # polytopes-lib
-a polytopes library in cpp
+
+A work in progress polytopes library in C++.
+Uses [sdl](http://www.libsdl.org/).
+
+# Development
+
+```bash
+# Clone the repository
+git clone https://github.com/invertime/polytopes-lib.git
+
+# Navigate into the folder, if not done.
+cd polytopes-lib
+
+# Build the project using make
+make
+```
+
+## License
+
+
+Distribué sous la licence GPLv3. Voir [`LICENSE`](LICENSE) pour plus d'informations.
---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <SDL2/SDL.h>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[DEBUG] > %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    SDL_Window *pWindow{nullptr};
+    SDL_Renderer *pRenderer{nullptr};
+    if (SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE, &pWindow, &pRenderer) < 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[DEBUG] > %s", SDL_GetError());
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    SDL_Event events;
+    bool isOpen{true};
+    while (isOpen)
+    {
+        while (SDL_PollEvent(&events))
+        {
+            switch (events.type)
+            {
+            case SDL_QUIT:
+                isOpen = false;
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, 255);
+        SDL_RenderClear(pRenderer);
+        SDL_RenderPresent(pRenderer);
+    }
+    SDL_DestroyRenderer(pRenderer);
+    SDL_DestroyWindow(pWindow);
+    SDL_Quit();
+    return EXIT_SUCCESS;
+}
